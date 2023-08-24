00bdecdf4a2855a831df1275bc0085e8c39e6fb6
Luiz Claudio <lzcldoliveira@gmail.com>
2023-08-24 11:07:22 -0300

    repetição

---
+++ aula13/ambiente.js
@@ -0,0 +1,27 @@
+/* Laços de repetição
+
+Comer uma pizza de 8 pedaços sozinho, até acabar a pizza! podemos fazer assim:
+console.log('Comer mais um pedaço...')
+console.log('Comer mais um pedaço...')
+console.log('Comer mais um pedaço...')
+console.log('Comer mais um pedaço...')
+console.log('Comer mais um pedaço...')
+console.log('Comer mais um pedaço...')
+console.log('Comer mais um pedaço...')
+console.log('Comer mais um pedaço...')
+OU */  
+
+/* Assim - While (Enquanto)
+var p = 1
+while (p <= 8) {
+    console.log('Comer mais uma fatia...')
+    p++ // c = c + 1
+} OU */
+
+// Assim - Do (Faça)
+var p = 1
+do {
+    console.log('Comer mais uma fatia...')
+    p ++
+} while (p <= 8)
+
