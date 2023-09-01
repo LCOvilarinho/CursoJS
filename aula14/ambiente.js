a6812fe21e5207a8f09a61a36fa519d5ea943c39
Luiz Claudio <lzcldoliveira@gmail.com>
2023-09-01 10:17:21 -0300

    com

---
+++ aula14/ambiente.js
@@ -0,0 +1,19 @@
+/* While
+var c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+
+// Do While
+var c = 1;
+ do {
+  console.log(c);
+  c++;
+} while (c <= 5)
+*/
+
+// For
+for (var c = 1; (c <= 5); c++) {
+    console.log(c)
+}
