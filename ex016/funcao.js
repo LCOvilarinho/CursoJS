c0108066a1505fc28a18e94334d77f8dbda9a82c
Luiz Claudio <lzcldoliveira@gmail.com>
2023-09-03 18:53:30 -0300

    funcao

---
+++ ex016/funcao.js
@@ -0,0 +1,16 @@
+// Par ou Impar
+function parimpar(n) {
+    if (n%2 == 0) {
+        return 'É Par!'
+    } else {
+        return 'É Ímpar!'
+    }
+
+}
+/* Ou assim:
+let res = parimpar(4)
+console.log(res)
+*/
+
+// Ou assim:
+console.log(parimpar(223))
